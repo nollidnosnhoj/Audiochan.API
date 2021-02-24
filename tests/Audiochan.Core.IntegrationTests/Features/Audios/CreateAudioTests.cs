4ea6e41faf2e9b5fa85ac394114a135a85ceae32
Dillon Johnson <nollidnosnhoj@gmail.com>
2021-02-24 01:53:22 -1000

    Update CreateAudioTests.cs

--- tests/Audiochan.Core.IntegrationTests/Features/Audios/CreateAudioTests.cs
+++ tests/Audiochan.Core.IntegrationTests/Features/Audios/CreateAudioTests.cs
@@ -15,7 +15,10 @@ namespace Audiochan.Core.IntegrationTests.Features.Audios
     {
         private readonly SliceFixture _fixture;
 
-        public CreateAudioTests(SliceFixture fixture) => _fixture = fixture;
+        public CreateAudioTests(SliceFixture fixture)
+        {
+            _fixture = fixture;
+        }
 
         [Fact]
         public async Task Should_Create_New_Audio()
@@ -24,16 +27,21 @@ namespace Audiochan.Core.IntegrationTests.Features.Audios
             var userId = await _fixture.RunAsDefaultUserAsync();
             var genre = new Genre {Name = "Dubstep", Slug = "dubstep"};
             await _fixture.InsertAsync(genre);
+            var uploadId = Guid.NewGuid();
             
             // ACT
             var result = await _fixture.SendAsync(new CreateAudioCommand
             {
-                UploadId = Guid.NewGuid(),
+                UploadId = uploadId,
                 FileName = "testaudio.mp3",
                 Duration = 100,
                 FileSize = 10000,
+                Title = "Test Audio",
+                Description = "This is a test audio",
                 Tags = new List<string>{ "apples", "oranges", "banana" },
-                Genre = "dubstep"
+                Genre = "dubstep",
+                IsLoop = true,
+                IsPublic = false
             });
 
             var created = await _fixture.ExecuteDbContextAsync(database =>
@@ -48,13 +56,19 @@ namespace Audiochan.Core.IntegrationTests.Features.Audios
             result.IsSuccess.Should().Be(true);
             result.Data.Should().NotBeNull();
             created.Should().NotBeNull();
-            created.Title.Should().Be("testaudio");
+            created.UploadId.Should().Be(uploadId);
+            created.Title.Should().Be("Test Audio");
+            created.Description.Should().Be("This is a test audio");
             created.FileExt.Should().Be(".mp3");
+            created.Duration.Should().Be(100);
+            created.FileSize.Should().Be(10000);
             created.Tags.Count.Should().Be(3);
             created.Tags.Should().Contain(x => x.Id == "apples");
             created.Tags.Should().Contain(x => x.Id == "oranges");
             created.Tags.Should().Contain(x => x.Id == "banana");
             created.Genre.Name.Should().Be("Dubstep");
+            created.IsPublic.Should().Be(false);
+            created.IsLoop.Should().Be(true);
             created.UserId.Should().Be(userId);
         }
     }
